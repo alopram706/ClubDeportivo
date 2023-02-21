c5a9ec7372f4023016e8460e61c965aa30f56408
PabloDV <pablodominguezvalle@gmail.com>
2023-02-21 14:44:45 +0100

    Fin -Comentarios

--- clubDeportivo/index.php
+++ clubDeportivo/index.php
@@ -5,7 +5,6 @@ require 'conexion.php';
 <html lang="es">
 
 <head>
-	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
